d92b079b0375d62ce729eacb844c0a0d7676c828
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-06 16:35:59 -0300

    adicionando estilo ao componente: Menu

--- src/components/Menu/index.jsx
+++ src/components/Menu/index.jsx
@@ -4,28 +4,29 @@ import maisCurtidas from '../../assets/icones/mais-curtidas-inativo.png'
 import maisVistas from '../../assets/icones/mais-vistas-inativo.png'
 import novas from '../../assets/icones/novas-inativo.png'
 import surpreendaMe from '../../assets/icones/surpreenda-me-inativo.png'
+import styles from './Menu.module.scss'
 
 export default function Menu() {
   return (
-    <nav>
-        <ul>
-            <li>
+    <nav className={styles.menu}>
+        <ul className={styles.menu__lista}>
+            <li className={styles.menu__item}>
                 <img src={home} alt=""/>
                 <a href="/">Início</a>
             </li>
-            <li>
+            <li className={styles.menu__item}>
                 <img src={maisCurtidas} alt=""/>
                 <a href="/">Mais curtidas</a>
             </li>
-            <li>
+            <li className={styles.menu__item}>
                 <img src={maisVistas} alt=""/>
                 <a href="/">Mais vistas</a>
             </li>
-            <li>
+            <li className={styles.menu__item}>
                 <img src={novas} alt=""/>
                 <a href="/">Novas</a>
             </li>
-            <li>
+            <li className={styles.menu__item}>
                 <img src={surpreendaMe} alt=""/>
                 <a href="/">Surpreenda-me</a>
             </li>
