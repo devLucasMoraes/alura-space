2b893ad2f2385d1a9e80cedc5f59d069f1194363
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-06 16:46:30 -0300

    adicionando banner e estilo a PaginaInicial

--- src/pages/PaginaInicial/index.jsx
+++ src/pages/PaginaInicial/index.jsx
@@ -1,11 +1,22 @@
 import Cabecalho from "../../components/Cabecalho";
 import Menu from "../../components/Menu";
+import banner from './banner.png'
+import styles from './PaginaInicial.module.scss'
+
 
 export default function PaginaInicial(){
     return (
         <>
             <Cabecalho/>
-            <Menu/>
+            <main>
+                <section className={styles.principal}>
+                    <Menu/>
+                    <div className={styles.principal__imagem}>
+                        <h1>A galeria mais completa do espaço</h1>
+                        <img src={banner} alt="A imagem da terra vista do espaço"/>
+                    </div>
+                </section>
+            </main>
         </>
     )
 }
