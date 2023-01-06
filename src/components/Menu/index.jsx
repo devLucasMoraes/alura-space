fd7e1153cb38e7b1dc874350ca8fb17c21e3980c
devLucasMoraes <devlucasmoraes@gmail.com>
2023-01-06 16:30:13 -0300

    criado componente: Menu

---
+++ src/components/Menu/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import home from '../../assets/icones/home-ativo.png'
+import maisCurtidas from '../../assets/icones/mais-curtidas-inativo.png'
+import maisVistas from '../../assets/icones/mais-vistas-inativo.png'
+import novas from '../../assets/icones/novas-inativo.png'
+import surpreendaMe from '../../assets/icones/surpreenda-me-inativo.png'
+
+export default function Menu() {
+  return (
+    <nav>
+        <ul>
+            <li>
+                <img src={home} alt=""/>
+                <a href="/">Início</a>
+            </li>
+            <li>
+                <img src={maisCurtidas} alt=""/>
+                <a href="/">Mais curtidas</a>
+            </li>
+            <li>
+                <img src={maisVistas} alt=""/>
+                <a href="/">Mais vistas</a>
+            </li>
+            <li>
+                <img src={novas} alt=""/>
+                <a href="/">Novas</a>
+            </li>
+            <li>
+                <img src={surpreendaMe} alt=""/>
+                <a href="/">Surpreenda-me</a>
+            </li>
+        </ul>
+    </nav>
+  )
+}
